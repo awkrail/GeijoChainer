e73ee2a18087e8c553513b308ba72d5a5375a28e
misogil0116 <bluecave339@icloud.com>
2017-05-31 14:04:18 +0900

    L.Classifierから分離

--- train.py
+++ train.py
@@ -58,6 +58,7 @@ def load_pickle():
 
     return af_imageData, af_labelData
 
+
 class SimpleAlex(chainer.Chain):
 	def __init__(self):
 		super(SimpleAlex, self).__init__(
@@ -76,62 +77,24 @@ class SimpleAlex(chainer.Chain):
 		return h
 
 
-class AlexNet(chainer.Chain):
-
-    def __init__(self):
-        super(AlexNet, self).__init__(
-            conv1 = L.Convolution2D(None, 96, 6, stride=3),
-            conv2 = L.Convolution2D(None, 256, 3, pad=2),
-            conv3 = L.Convolution2D(None, 384, 3, pad=1),
-            conv4 = L.Convolution2D(None, 384, 3, pad=1),
-            conv5 = L.Convolution2D(None, 384, 2, pad=1),
-            conv6 = L.Convolution2D(None, 384, 3, pad=1),
-            conv7 = L.Convolution2D(None, 384, 3, pad=1),
-            conv8 = L.Convolution2D(None, 256, 3, pad=1),
-            fc6 = L.Linear(None, 6400),
-            fc7 = L.Linear(None, 1000),
-            fc8 = L.Linear(None, 11),
-            #fc9 = L.Linear(None, 20)
-        )
-
-    def __call__(self, x):
-        h = F.max_pooling_2d(F.local_response_normalization(F.relu(self.conv1(x))), 3, stride=1)
-        h = F.max_pooling_2d(F.local_response_normalization(F.relu(self.conv2(h))), 3, stride=1)
-        h = F.relu(self.conv3(h))
-        h = F.relu(self.conv4(h))
-        h = F.max_pooling_2d(F.relu(self.conv5(h)), 2, stride=1)
-        h = F.relu(self.conv6(h))
-        h = F.relu(self.conv7(h))
-        h = F.max_pooling_2d(F.relu(self.conv8(h)), 3, stride=2)
-        h = F.dropout(F.relu(self.fc6(h)))
-        h = F.dropout(F.relu(self.fc7(h)))
-        h = self.fc8(h)
-
-        return h
-
-
 # モデルのインスタンス化
-model = L.Classifier(SimpleAlex())
+model = SimpleAlex()
+classify_model = L.Classifier(model)
 optimizer = chainer.optimizers.Adam()
-optimizer.setup(model)
+optimizer.setup(classify_model)
 
-# 訓練データとテストデータに分割
-# train, test = chainer.datasets.get_cifar10()
-# chainer.datasets.get_cifar10()で渡されるデータの次元は(3,32,32)
-# 自分がとってきたデータは(3*1024)
 
 imageData, labelData = load_pickle()
 threshold = np.int32(len(imageData)/5*4)
 train = tuple_dataset.TupleDataset(imageData[0:threshold], labelData[0:threshold])
 test = tuple_dataset.TupleDataset(imageData[threshold:], labelData[threshold:])
 
-# train, test = chainer.datasets.get_cifar10()
 train_iter = chainer.iterators.SerialIterator(train, 100, shuffle=True)
 test_iter = chainer.iterators.SerialIterator(test, 100, repeat=False, shuffle=False)
 
 updater = training.StandardUpdater(train_iter, optimizer, device=0)
 trainer = training.Trainer(updater, (40, 'epoch'), out='Geijo_result')
-trainer.extend(extensions.Evaluator(test_iter, model, device=0))
+trainer.extend(extensions.Evaluator(test_iter, classify_model, device=0))
 trainer.extend(extensions.dump_graph('main/loss'))
 trainer.extend(extensions.LogReport())
 trainer.extend(extensions.PlotReport(y_keys='main/loss', file_name='loss.png'))
